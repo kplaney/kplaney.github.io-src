379df41fb3d8a61da8f5215c50e7ef8c0f90cfa0
kplaney <catherine.planey@gmail.com>
2018-01-14 13:25:42 -0800

    added ABOUT_ME page

--- pelicanconf.py
+++ pelicanconf.py
@@ -64,3 +64,7 @@ BOOTSTRAP_THEME='flatly'
 GITHUB_USER = 'kplaney'
 PYGMENTS_STYLE = 'monokai'
 
+
+ABOUT_ME = 'I am an entrepreneur and co-Founder based in Northern California (Silicon Valley, if you will). My technical background is biomedical informatics (if you want to get fancy, that's machine learning and statistics applied to medical data.) I am interested in helping build the next-generation biotech companies that will ultimately change what current pharma looks like today. I keep this blog to share interesting tidbits and musings along my journey.  
+
+While I make an honest effort to write my thoughts in an interpretable format on my first draft, I do not use spelling or grammar check tools on this site, and I do not spend hours crafting my posts. Life is a little too busy as an entrepreneur for that!'
